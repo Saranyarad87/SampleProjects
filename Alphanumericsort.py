901cd8bbb4636adffdb9c828c9859fa4c6fd3265
Saranyarad87 <52082801+Saranyarad87@users.noreply.github.com>
2019-06-23 16:29:47 -0400

    Add files via upload

---
+++ Alphanumericsort.py
@@ -0,0 +1,82 @@
+
+    
+import re
+
+class Alphanumsort():
+    
+    def __init__(self):
+         
+        self.sortedstring =''
+        self.strval = input("enter alphanumeric string: \n")
+
+# check digit
+    def chk_digit(self,val):
+        if (val.isdigit()):
+            return int(val)
+        else:
+            return (val)
+
+# split into a list
+        
+    def mysplit(self,s):
+              
+        return re.split('([0-9]+)',s)
+
+# Split string into numbers,letters and special charachters
+
+    def splitString_sort(self,chk_str):
+        
+        lower = []
+        upper = []
+        number = []
+        specialchar = []
+        
+        for i in range(len(chk_str)):
+                          
+            if (chk_str[i].isdigit()):
+                number.append(chk_str[i])
+                    
+            elif(chk_str[i] >= 'A' and chk_str[i] <= 'Z'): 
+                
+                upper += chk_str[i] 
+            
+            elif(chk_str[i] >= 'a' and chk_str[i] <= 'z'):
+                
+                lower +=chk_str[i]
+            else: 
+                specialchar += chk_str[i] 
+       
+        sort_list = list(map(int,number))
+        
+        if (sort_list.sort() != 0):
+        
+            return(sort_list),(sorted(lower,reverse=True)),(sorted(upper,reverse=True)),(sorted(specialchar,reverse=True)) 
+        
+        else:
+            
+            print ("error while sorting numbers")
+  
+ # merge sorted list and create final alphanumeric string
+    
+    def create_alnumstr(self):
+        
+        str_to_process = self.mysplit(self.strval)
+        n,a,A,s = (self.splitString(str_to_process))
+        numlist = list(map(str,n))
+        s1 =(''.join(numlist))
+        s2 =(''.join(a))
+        s3 =(''.join(A))
+        s4 =(''.join(s))
+        final_list = s1+s2+s3+s4
+        return final_list
+           
+
+# main program
+            
+if __name__ == '__main__':
+            
+    x = Alphanumsort()          
+    
+    print ("sorted string is: %s" %x.create_alnumstr())
+
+
---
+++ bubblesort.py
@@ -0,0 +1,115 @@
+
+#Bubble sort in python
+# takes both numbers and alphabets
+#can sort ascending/descending or alphabetically
+
+class Bsort():
+    
+    def __init__(self):
+        
+        self.str_format = 0
+        self.str_to_sort = ''
+        self.sort_list=  []
+        self.str_length = 0
+        self.order = ''
+        
+    def getinfo(self):
+        
+        try:
+            
+            self.str_format = input ("Welcome to the bubble sort program.What would you like to sort today? \n 1.Numbers \n 2.Alphabets \n\n")
+           
+        
+            if ( self.str_format == '1'):
+                
+                self.str_to_sort = input("Please enter numbers here with spaces: ")
+                sort_list=  self.str_to_sort.split()
+                self.sort_list = list(map(int,sort_list))
+                self.str_length = len(self.sort_list)
+                self.order = input ("How would you like to sort the numbers.Please type 'a' for ascending or 'd' for descending: ")
+                if (self.order.lower() in ['a','d']):
+                    pass
+                else:
+                    print ( "sorting order selected is not valid, defaulting to ascending order")
+                    self.order = 'a'
+                    
+               
+            elif (self.str_format == '2'):
+                
+                self.str_to_sort = input("Please enter alphabets here with spaces: ")
+                self.sort_list=  self.str_to_sort.split()
+                self.str_length = len(self.sort_list)
+                self.order = input ("Would you like to sort alphabetically? (y/n) :")
+        
+                if (self.order.lower() in ['y','n']):
+                    pass
+                else:
+                    print ("sorting order has not been selected, defaulting to alphabetical order")
+                    self.order = 'y'
+                    
+            else:
+                
+                print ("Please select option 1 or 2 to try out the sorting.")
+                raise Exception
+            
+            return True
+        except:
+            print ("Please re-run the program.")
+            return False
+    
+    def bubble_sort(self):
+        
+        try:
+            
+        # iterate through the list and swap values 
+            if (self.getinfo() ==  True):
+       
+                if (self.order.lower() in ['d','n']):
+                   
+                    for x in range (self.str_length-1):
+                        for y in range (self.str_length-x-1):
+                            if self.sort_list[y] < self.sort_list[y+1]:
+                                self.sort_list[y],self.sort_list[y+1] = self.sort_list[y+1],self.sort_list[y]
+
+                    return (self.sort_list)
+                    
+                elif (self.order.lower() in ['a','y']): 
+                    
+                    for x in range (self.str_length-1):
+                        for y in range (self.str_length-x-1):
+                            if self.sort_list[y] > self.sort_list[y+1]:
+                                self.sort_list[y],self.sort_list[y+1] = self.sort_list[y+1],self.sort_list[y]
+                   
+                    return (self.sort_list)
+                
+                else:
+                    
+                    raise Exception
+            
+            else:
+                
+                print ("Exiting...")
+                raise Exception
+            
+        except:
+            
+            return 0
+          
+
+#Main program
+
+if __name__ == '__main__':
+    
+    try:
+        
+        s = Bsort()
+        print (s.bubble_sort())
+    
+    except:
+        
+        print ("Error occured in main program.")
+    
+    
+    
+    
+
